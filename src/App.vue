5de398a0eec67b25587b886622dd3b3eaf8bcafb
brtkuu <bartoszposert5@gmail.com>
2021-06-24 15:59:34 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,54 @@
 <template>
-  <div id="app">
+  <div class="app">
+    <div class="banner-top__container">
+    <img class="banner-top__logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rick_and_Morty.svg/1280px-Rick_and_Morty.svg.png" alt="rick&mortylogo" />
+    <input class="banner-top__input" type="text" placeholder="Start typing to search..." />
+    </div>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">All characters</router-link> |
+      <router-link to="/about">Favorites</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
 }
+</script>
 
-#nav {
-  padding: 30px;
+<style>
 
-  a {
+</style>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap');
+.app {
+  max-width: 1200px;
+  margin: auto;
+  font-family: 'Poppins', sans-serif;
+}
+.banner-top{
+  &__logo{
+    height: 70px;
+    width: 240px;
+  }
+  &__container {
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    height: 150px;
+    max-width: 1200px;
+    margin: auto;
+  }
+  &__input {
+    height: 40px;
+    width: 500px;
+    font-size: 18px;
+    border-radius: 10px;
+    padding: 10px;
+    margin-left: 50px;
     font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,31 @@
-import Vue from 'vue';
-import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
+/* eslint-disable */
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Allcharacters from "../views/Allcharacters.vue";
+import Favorites from "../views/Favorites.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: "/",
+    name: "characters",
+    component: Allcharacters
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "favorites",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+    component: Favorites
+  }
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes
 });
 
 export default router;
